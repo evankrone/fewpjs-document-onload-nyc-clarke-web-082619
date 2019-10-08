3f6d5c7f04c5b0309160b20bef2a53476d60c61d
Evan Krone <evankrone@Evans-MacBook-Pro-2.local>
2019-10-08 10:05:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function(){
-    e()
-});
-
 function e() {
 document.getElementById('text').innerHTML = 'This is really cool!';
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+    e()
+});
 
